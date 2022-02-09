db77df14299b922e78e1cb550fe88647739d4408
BBailey1985 <brandon_bailey@hotmail.com>
2022-02-09 13:22:54 -0600

    added Post model and set up express router

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS all_things_tech_db;
+
+CREATE DATABASE all_things_tech_db;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
+// const routes = require('./controllers');
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const session = require('express-session');
+
 
 const sess = {
   secret: 'its a big secret',
@@ -22,10 +23,10 @@ const PORT = process.env.PORT || 3001
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars');
-app.set('view engine', 'handlebars');
+// app.engine('handlebars');
+// app.set('view engine', 'handlebars');
 app.use(session(sess));
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
